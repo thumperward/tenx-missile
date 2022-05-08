429152b3113fd4051ba2e3986e92e1ff041b1442
thumperward <thumperward@hotmail.com>
2022-05-08 14:57:29 +0200

    move test code into function

--- tests/test_fire.py
+++ tests/test_fire.py
@@ -2,13 +2,14 @@ from tenx_missile import MissileLauncher
 import time
 
 
-m = MissileLauncher()
-m.left(1500)
-time.sleep(2)
-m.right(1500)
-time.sleep(2)
-m.down(1500)
-time.sleep(2)
-m.up(1500)
-time.sleep(2)
-m.fire()
+def test_file():
+    m = MissileLauncher()
+    m.left(1500)
+    time.sleep(2)
+    m.right(1500)
+    time.sleep(2)
+    m.down(1500)
+    time.sleep(2)
+    m.up(1500)
+    time.sleep(2)
+    m.fire()
